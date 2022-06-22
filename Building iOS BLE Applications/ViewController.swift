86856eb84d1f4235fd7fedd4ed39ab99ef3bb262
Samuel Noye <samuel.noye@amalitech.org>
2022-06-22 16:33:57 +0000

    Initial Commit

---
+++ Building iOS BLE Applications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Building iOS BLE Applications
+//
+//  Created by Samuel Noye on 22/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
