9df15f312ec0377bf1b157909e3e3db5b1387e5e
Samuel Noye <samuel.noye@amalitech.org>
2022-06-23 13:22:10 +0000

    setup

--- Building iOS BLE Applications/AppDelegate.swift
+++ Building iOS BLE Applications/AppDelegate.swift
@@ -11,15 +11,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .blue
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
         return true
     }
 
