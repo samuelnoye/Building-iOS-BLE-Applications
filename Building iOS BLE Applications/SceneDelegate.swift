5a191bb7e662825f9ea7a8af37b97474d2485a87
Samuel Noye <samuel.noye@amalitech.org>
2022-07-06 18:27:27 +0000

    Update SceneDelegate.swift

--- Building iOS BLE Applications/SceneDelegate.swift
+++ Building iOS BLE Applications/SceneDelegate.swift
@@ -35,7 +35,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+// 
     // func sceneWillEnterForeground(_ scene: UIScene) {
     //     // Called as the scene transitions from the background to the foreground.
     //     // Use this method to undo the changes made on entering the background.
